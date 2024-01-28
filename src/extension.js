b7a780391ffc4ebbc4f6de97e9791fbfab5f453a
satoshi.yamamoto <standard.software.net@gmail.com>
2024-01-28 13:00:38 +0900

    add select month

--- src/extension.js
+++ src/extension.js
@@ -12,13 +12,13 @@ const {
 const {
   // isUndefined,
   // _dateToString,
-  // _Year,
-  // _Month,
+  _Year,
+  _Month,
   _Day,
 } = require(`./parts/parts.js`);
 
 const {
-  // equalMonth,
+  equalMonth,
   // equalDate,
   // equalToday,
   // monthDayCount,
@@ -131,6 +131,183 @@ const selectCalendarType = ({
   );
 };
 
+const selectMonth = ({
+  editor, dateToString
+}) => {
+
+  const selectDateRange200Year = () => {
+
+    const dateThisYear = _Year(`this`);
+    const dateLastYear = _Year(-1, dateThisYear);
+    const dateNextYear = _Year(+1, dateThisYear);
+
+    const yearThis =  dateThisYear.getFullYear();
+    commandQuickPick([
+      {
+        label: `${yearThis - 100} - ${yearThis - 10 - 1} : 100 year before`,
+        description: `▸`,
+        func: () => { selectTenYear(_Year(-100, dateThisYear)); }
+      },
+      {
+        label: `${yearThis - 10} - ${yearThis - 2} : 10 year before`,
+        description: `▸`,
+        func: () => { selectOneYear(_Year(-10, dateThisYear), 9); }
+      },
+      {
+        label: `${yearThis - 1} : Last year`,
+        description: `▸`,
+        func: () => { selectMonthInYear(dateLastYear); }
+      },
+      {
+        label: `${yearThis} : This year`,
+        description: `▸`,
+        func: () => { selectMonthInYear(dateThisYear); }
+      },
+      {
+        label: `${yearThis + 1} : Next year`,
+        description: `▸`,
+        func: () => { selectMonthInYear(dateNextYear); }
+      },
+      {
+        label: `${yearThis + 2} - ${yearThis + 10} : 10 year after`,
+        description: `▸`,
+        func: () => { selectOneYear(_Year(2, dateThisYear), 9); }
+      },
+      {
+        label: `${yearThis + 10 + 1} - ${yearThis + 100} : 100 year after`,
+        description: `▸`,
+        func: () => { selectTenYear(_Year(11, dateThisYear)); }
+      },
+    ], `Smart Insert Date | Select Date | Select Year range -100 to +100`);
+  }
+
+  const selectTenYear = (dateYear) => {
+    const commands = [];
+    for (let i = 0; i <= 8; i += 1) {
+      const targetDate = _Year(i * 10, dateYear);
+      commands.push({
+        label: `${dateToString(targetDate, `YYYY`)} - ${dateToString(_Year(9, targetDate), `YYYY`)}`,
+        description: `▸`,
+        func: () => { selectOneYear(targetDate, 10); },
+      });
+    }
+    commandQuickPick(commands,
+      `Smart Insert Date | Select Date | ` +
+      `${dateToString(dateYear, `YYYY`)} - ${dateToString(_Year(89, dateYear), `YYYY`)}`);
+  };
+
+  const selectOneYear = (dateYear, count) => {
+    const commands = [];
+    for (let i = 0; i <= count - 1; i += 1) {
+      const targetDate = _Year(i, dateYear);
+      commands.push({
+        label: dateToString(targetDate, `YYYY`),
+        description: `▸`,
+        func: () => { selectMonthInYear(targetDate); },
+      });
+    }
+    commandQuickPick(commands,
+      `Smart Insert Date | Select Date | ` +
+      `${dateToString(dateYear, `YYYY`)} - ${dateToString(_Year(9, dateYear), `YYYY`)}`);
+  };
+
+  const selectMonthInYear = (dateYear) => {
+    const commands = [];
+    for (let i = 1; i <= 12; i += 1) {
+      const targetDate = _Month(i - 1, dateYear);
+      const isThisMonth = equalMonth(targetDate, _Month(`this`));
+      commands.push({
+        label: dateToString(targetDate, `MM : YYYY-MM : MMM`) +
+        (isThisMonth ? ` : This month` : ``),
+        description: `▸`,
+        func: () => {
+          selectCalendarType({
+            editor, dateToString,
+            targetDate,
+            placeHolder: `Insert Calendar : This Month : Select Calendar Type`,
+            calendarTypes: [`Square`, `Vertical`],
+          });
+        },
+      });
+    }
+    commandQuickPick(commands,
+      `Smart Insert Date | Select Date | ` +
+      `${dateYear.getFullYear()}`);
+  };
+
+  const dateThisYear = _Year(`this`);
+  const dateLastYear = _Year(-1, dateThisYear);
+  const dateNextYear = _Year(+1, dateThisYear);
+
+  const dateThisMonth = _Month(`this`);
+  const dateLastMonth = _Month(-1, dateThisMonth);
+  const dateNextMonth = _Month(+1, dateThisMonth);
+
+  commandQuickPick([
+    {
+      label: `This Month | `
+      + `${dateToString(dateThisMonth, `YYYY-MM : MMM`)}`,
+      description: `▸`,
+      func: () => {
+        selectCalendarType({
+          editor, dateToString,
+          targetDate: dateThisMonth,
+          placeHolder: `Insert Calendar : This Month : Select Calendar Type`,
+          calendarTypes: [`Square`, `Vertical`],
+        });
+      }
+    },
+    {
+      label: `-1 Month | Last Month | `
+      + `${dateToString(dateLastMonth, `YYYY-MM : MMM`)}`,
+      description: `▸`,
+      func: () => {
+        selectCalendarType({
+          editor, dateToString,
+          targetDate: dateLastMonth,
+          placeHolder: `Insert Calendar : Last Month : Select Calendar Type`,
+          calendarTypes: [`Square`, `Vertical`],
+        });
+      }
+    },
+    {
+      label: `+1 Month | Next Month | `
+      + `${dateToString(dateNextMonth, `YYYY-MM : MMM`)}`,
+      description: `▸`,
+      func: () => {
+        selectCalendarType({
+          editor, dateToString,
+          targetDate: dateNextMonth,
+          placeHolder: `Insert Calendar : Next Month : Select Calendar Type`,
+          calendarTypes: [`Square`, `Vertical`],
+        });
+      }
+    },
+    {label: ``, kind: vscode.QuickPickItemKind.Separator},
+    {
+      label: `This Year | ${dateThisYear.getFullYear()}`,
+      description: `▸`,
+      func: () => { selectMonthInYear(dateThisYear); }
+    },
+    {
+      label: `-1 Year | Last Year | ${dateLastYear.getFullYear()}`,
+      description: `▸`,
+      func: () => { selectMonthInYear(dateLastYear); }
+    },
+    {
+      label: `+1 Year | Next Year | ${dateNextYear.getFullYear()}`,
+      description: `▸`,
+      func: () => { selectMonthInYear(dateNextYear); }
+    },
+    {label: ``, kind: vscode.QuickPickItemKind.Separator},
+    {
+      label: `Select Year range -100 to +100`,
+      description: `▸`,
+      func: () => { selectDateRange200Year(); }
+    },
+  ], `Smart Insert Date | Select Date`);
+}
+
 const extensionMain = (commandName) => {
   const editor = getEditor(); if (!editor) { return; }
 
@@ -146,15 +323,13 @@ const extensionMain = (commandName) => {
         otherMonthDate: false,
         startDayOfWeek: "Sun"
       };
-      const targetDate = new Date();
-      const calendarText = textCalendarMonthly(
-        {
-          targetDate,
+      insertText(editor,
+        textCalendarMonthly({
+          targetDate: new Date(),
           dateToString,
           ...setting,
-        }
-      );
-      insertText(editor, calendarText)
+        })
+      )
     }; break;
 
     case `VerticalCalendarThisMonth`: {
@@ -163,17 +338,15 @@ const extensionMain = (commandName) => {
         lineFormat: "  DD ddd",
         startDayOfWeek: "Sun"
       };
-      const targetDates = getDateArrayWeeklyMonth(
-        _Day(`today`), setting.startDayOfWeek
-      );
-      const calendarText = textCalendarLineVertical(
-        {
-          targetDates,
+      insertText(editor,
+        textCalendarLineVertical({
+          targetDates: getDateArrayWeeklyMonth(
+            _Day(`today`), setting.startDayOfWeek
+          ),
           dateToString,
           ...setting,
-        }
-      );
-      insertText(editor, calendarText)
+        })
+      )
     }; break;
 
     case `SelectCalendarType`: {
@@ -185,35 +358,35 @@ const extensionMain = (commandName) => {
       });
     }; break;
 
+    case `SelectMonth`: {
+      selectMonth({
+        editor, dateToString,
+      });
+    }; break;
   }
-
-
 };
 
 function activate(context) {
 
   registerCommand(context,
     `vscode-insert-calendar.SquareCalendarThisMonth`,
-    () => {
-      extensionMain(`SquareCalendarThisMonth`);
-    }
+    () => { extensionMain(`SquareCalendarThisMonth`); }
   );
 
   registerCommand(context,
     `vscode-insert-calendar.VerticalCalendarThisMonth`,
-    () => {
-      extensionMain(`VerticalCalendarThisMonth`);
-    }
+    () => { extensionMain(`VerticalCalendarThisMonth`); }
   );
 
   registerCommand(context,
     `vscode-insert-calendar.SelectCalendarType`,
-    () => {
-      extensionMain(`SelectCalendarType`);
-    }
+    () => { extensionMain(`SelectCalendarType`); }
   );
 
-
+  registerCommand(context,
+    `vscode-insert-calendar.SelectMonth`,
+    () => { extensionMain(`SelectMonth`); }
+  );
 }
 
 function deactivate() {}
